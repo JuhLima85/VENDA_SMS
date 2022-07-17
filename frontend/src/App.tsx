dee11b9929862cb8ebd8278aae1ebc8750748d23
JuhLima85 <juju_amil@hotmail.com>
2022-07-17 18:36:09 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButtom from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButtom />
+    </>
   )
 }
 
