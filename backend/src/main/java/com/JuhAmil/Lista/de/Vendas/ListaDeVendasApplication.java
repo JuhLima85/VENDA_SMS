1941da2e4b451724e58f19ad59e20cc46bd2da72
JuhLima85 <juju_amil@hotmail.com>
2022-07-17 15:29:20 -0300

    Criacao do projeto

---
+++ backend/src/main/java/com/JuhAmil/Lista/de/Vendas/ListaDeVendasApplication.java
@@ -0,0 +1,13 @@
+package com.JuhAmil.Lista.de.Vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/JuhAmil/Lista/de/Vendas/ListaDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuhAmil.Lista.de.Vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
