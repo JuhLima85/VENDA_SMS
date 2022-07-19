9068f3db22d2f80b5b46868f34ae00c1f7082c13
JuhLima85 <juju_amil@hotmail.com>
2022-07-19 14:51:48 -0300

    Banco de dados

---
+++ backend/src/main/java/com/JuhAmil/Lista/de/Vendas/entidades/Vendas.java
@@ -0,0 +1,79 @@
+package com.JuhAmil.Lista.de.Vendas.entidades;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_vendas")
+public class Vendas {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String vendedor;
+	private Integer visitas;
+	private double venda;
+	private Integer total;
+	private LocalDate data;
+	
+	public Vendas() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(String vendedor) {
+		this.vendedor = vendedor;
+	}
+
+	public Integer getVisitas() {
+		return visitas;
+	}
+
+	public void setVisitas(Integer visitas) {
+		this.visitas = visitas;
+	}
+
+	public double getVenda() {
+		return venda;
+	}
+
+	public void setVenda(double venda) {
+		this.venda = venda;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+	
+
+}
