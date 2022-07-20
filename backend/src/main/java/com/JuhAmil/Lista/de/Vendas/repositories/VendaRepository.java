b150c92640529d4cc6d2fa1fbe116a713fc78c31
JuhLima85 <juju_amil@hotmail.com>
2022-07-20 11:42:10 -0300

    Selecao de data

--- backend/src/main/java/com/JuhAmil/Lista/de/Vendas/repositories/VendaRepository.java
+++ backend/src/main/java/com/JuhAmil/Lista/de/Vendas/repositories/VendaRepository.java
@@ -1,9 +1,17 @@
 package com.JuhAmil.Lista.de.Vendas.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.JuhAmil.Lista.de.Vendas.entidades.Venda;
 
 public interface VendaRepository extends JpaRepository<Venda, Long>{
+	
+	@Query("SELECT obj FROM Venda obj WHERE obj.data BETWEEN :min AND :max ORDER BY obj.total DESC")
+	Page<Venda> findVendas(LocalDate min, LocalDate max, Pageable pageable);
 
 }
--- backend/src/main/java/com/JuhAmil/Lista/de/Vendas/services/VendaService.java
+++ backend/src/main/java/com/JuhAmil/Lista/de/Vendas/services/VendaService.java
@@ -1,8 +1,12 @@
 package com.JuhAmil.Lista.de.Vendas.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.JuhAmil.Lista.de.Vendas.entidades.Venda;
@@ -14,8 +18,35 @@ public class VendaService {
 	@Autowired
 	private VendaRepository repository;
 	
-	public List<Venda> buscarVendas() {
-		return repository.findAll();
+	public Page<Venda> buscarVendas(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate hoje = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate mim =  minDate.equals("") ? hoje.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? hoje : LocalDate.parse(maxDate);
+		
+		return repository.findVendas(mim, max, pageable);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
